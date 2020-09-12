423df110a6eefca289979c793f30d1050c424d3f
RenSUGANO <jianyelian18@gmail.com>
2020-09-12 07:19:57 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -16,3 +16,10 @@
         </div>
     </nav>
 </header>
+
+        <ul class="nav navbar-nav navbar-right">
+            {{-- ユーザ登録ページへのリンク --}}
+            <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+            {{-- ログインページへのリンク --}}
+            <li><a href="#">Login</a></li>
+        </ul>
