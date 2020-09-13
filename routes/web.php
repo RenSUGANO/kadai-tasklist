ac002b9a777267baf83cc3cd59369d33b6a7a068
RenSUGANO <jianyelian18@gmail.com>
2020-09-13 02:02:58 +0000

    modified routes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('sign
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 // 認証
-Route::get('login', 'Auth\LoginController@shoqLoginForm')->name('login');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
